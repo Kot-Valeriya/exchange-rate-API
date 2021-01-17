b3b48962e70d6becc204a3dc8efd00b172d53422
Valeriya Kot <kot.valeriia@gmail.com>
2021-01-17 16:41:40 +0200

    Command added to update rates

---
+++ src/Command/UpdateExchangeRatesCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Command;
+
+use App\ExchangeRateHelper;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class UpdateExchangeRatesCommand extends Command {
+	protected static $defaultName = 'app:update-exchange-rates';
+	private $entityManager;
+	private $helper;
+
+	public function __construct(EntityManagerInterface $entityManager, ExchangeRateHelper $helper) {
+		parent::__construct();
+		$this->entityManager = $entityManager;
+		$this->helper = $helper;
+	}
+
+	protected function configure() {
+		$this
+			->setDescription('Updates exchange rates.')
+			->setHelp('This command allows you to update exchange rates.')
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output): int{
+
+		$io = new SymfonyStyle($input, $output);
+		$date = new \DateTime('now');
+		$exchangeRatesArray = $this->helper->getExchangeRatesArr($date->format('Y-m-d'));
+		foreach ($exchangeRatesArray as $exchangeRate) {
+			$this->entityManager->persist($exchangeRate);
+		}
+		$this->entityManager->flush();
+
+		$io->success('Exchange rates updated successfully');
+		return Command::SUCCESS;
+	}
+}
